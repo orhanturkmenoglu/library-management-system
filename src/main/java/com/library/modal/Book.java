820c5ba4460c36da091a345379f12ba63072e17f
orhan <orhantrkmn749@gmail.com>
2026-01-28 02:36:41 +0300

    refactor: code cleanup and small fixes

--- src/main/java/com/library/modal/Book.java
+++ src/main/java/com/library/modal/Book.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.AssertTrue;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.hibernate.type.SqlTypes;
 
