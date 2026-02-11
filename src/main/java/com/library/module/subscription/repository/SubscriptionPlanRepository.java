93375e61f53f40b1b649b4e623349910c3b36fb8
orhan <orhantrkmn749@gmail.com>
2026-02-11 18:10:09 +0300

    feat: SubscriptionPlan için DTO, mapper ve service katmanları eklendi; repository ismi güncellendi

--- src/main/java/com/library/module/subscription/repository/SubscriptionPlanRepository.java
+++ src/main/java/com/library/module/subscription/repository/SubscriptionPlanRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SubscriptionPlan extends JpaRepository<SubscriptionPlan,Long> {
+public interface SubscriptionPlanRepository extends JpaRepository<SubscriptionPlanRepository,Long> {
+
 }
